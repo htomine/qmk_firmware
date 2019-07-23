815cb238b8c9d5f15aaabfa76f3a559e35948eac
hsgw <urkwtky@gmail.com>
2019-07-23 18:14:47 +0900

    fix hsgw keymap

--- keyboards/lain/keymaps/hsgw/keymap.c
+++ keyboards/lain/keymaps/hsgw/keymap.c
@@ -21,6 +21,7 @@
 #define S_SPC   LSFT_T(KC_SPC)
 #define GUI_ZH  LGUI_T(JP_ZHTG)
 #define FUNC_BS LT(FUNC,KC_BSPC)
+#define TAB_CTL LCTL_T(KC_TAB)
 
 // Defines the keycodes used by our macros in process_record_user
 // enum custom_keycodes {
@@ -35,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     JP_AT, \
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    JP_SCLN,  JP_QUOT,\
+        TAB_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    JP_SCLN,  JP_COLN,\
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_INS, KC_N,    KC_M,    JP_COMM, JP_DOT,   JP_SLSH, \
         KC_LCTL,                   KC_LALT, GUI_ZH,  MO(NUM), KC_SPC,    KC_ENT, KC_BSPC, MO(FUNC),KC_MENU, KC_RCTL,  JP_BSLS\
     ),
@@ -46,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,                   _______, _______, _______, _______,   _______, _______, _______, _______, _______,  _______ \
     ),
     [FUNC] = LAYOUT(
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
+        _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
         _______, _______, _______, _______, _______, _______, _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
         _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, \
         _______,                   _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______ \
