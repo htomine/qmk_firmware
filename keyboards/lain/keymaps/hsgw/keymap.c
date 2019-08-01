00bee5774e019237837a7171ea87baec1fe0f19c
hsgw <urkwtky@gmail.com>
2019-08-01 12:54:10 +0900

    update keymap

--- keyboards/lain/keymaps/hsgw/keymap.c
+++ keyboards/lain/keymaps/hsgw/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     JP_AT, \
         TAB_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_RBRC,   KC_H,   KC_J,    KC_K,    KC_L,    JP_SCLN,  JP_COLN,\
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_INS, KC_N,    KC_M,    JP_COMM, JP_DOT,   JP_SLSH, \
-        KC_LCTL,                   KC_LALT, GUI_ZH,  MO(NUM), KC_SPC,    KC_ENT, KC_BSPC, MO(FUNC),KC_MENU, KC_RCTL,  JP_BSLS\
+        KC_LCTL,                   KC_LALT, GUI_ZH,  MO(NUM), KC_SPC,    KC_ENT, KC_BSPC, MO(FUNC),KC_MENU, JP_CIRC,  JP_BSLS\
     ),
     [NUM] = LAYOUT(
         KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    JP_CIRC,  JP_YEN, \
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [FUNC] = LAYOUT(
         _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-        _______, _______, _______, _______, _______, _______, _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
-        _______, _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______, _______, \
-        _______,                   _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______ \
+        _______, _______, _______, _______, _______, _______, _______,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+        _______, _______, _______, _______, _______, _______,             _______, KC_PGDN, KC_PGUP, _______, _______, _______, \
+        _______,                   _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______ \
     ),
     [CONF] = LAYOUT(
         RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
